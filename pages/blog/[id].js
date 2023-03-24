07b175ed9d74f737f31ded07c4e701a881015e40
Tarik Tahirovic <tariktahirovic@rezolve.com>
2023-03-24 16:10:57 +0100

    update blog

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,23 +1,30 @@
 import { useRouter } from 'next/router';
 import Layout from '@/components/Layout';
 import CustomButton from '@/components/button/CustomButton';
+import {css} from "@emotion/css";
+
+const imgStyles = css`
+  display: block;
+  margin: 0 auto;
+`;
 
 const BlogPost = () => {
     const router = useRouter();
-    const { id, content, title } = router.query;
+    const { id, content, title, image } = router.query;
 
     const handleBackButtonClick = () => {
         router.back();
     };
 
-    const post = { id, title, content };
+    const post = { id, title, content, image };
 
     return (
         <Layout>
             <h1>{post.title}</h1>
-            <CustomButton variant={'contained'} onClick={handleBackButtonClick}>
+            <CustomButton variant={'contained'} onClick={handleBackButtonClick} width={'100%'}>
                 Back
             </CustomButton>
+            <img src={post.image}  alt={post.image} className={imgStyles}/>
             <p>{post.content}</p>
         </Layout>
     );
--- pages/countries-api.js
+++ pages/countries-api.js
@@ -2,89 +2,102 @@ import { ApolloClient, ApolloProvider, InMemoryCache, useQuery } from '@apollo/c
 import gql from 'graphql-tag';
 import React, { useState } from 'react';
 import Layout from '@/components/Layout';
+import { keyframes } from '@emotion/react';
+import {css} from "@emotion/css";
 
-export const client = new ApolloClient({
-  uri: 'https://countries.trevorblades.com/graphql',
-  cache: new InMemoryCache(),
-});
-
-const GET_COUNTRIES = gql`
-  {
-    countries {
-      languages {
-        name
-      }
-      name
-      currency
-      code
-      phone
-      capital
-      states {
-        name
-      }
-    }
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(-100%);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0%);
   }
 `;
 
+const countryStyles = css`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+const listStyles = css`
+  width: 50%;
+`;
+
+const listItemStyles = (selected) => css`
+  color: ${selected ? 'blue' : 'none'};
+  font-family: Inter,sans-serif;
+  cursor: pointer;
+`;
+
+const selectedCountryStyles = css`
+  font-family: Inter,sans-serif;
+  animation: ${fadeIn} 1s;
+`;
+
+export const client = new ApolloClient({
+    uri: 'https://countries.trevorblades.com/graphql',
+    cache: new InMemoryCache(),
+});
+
+const GET_COUNTRIES = gql`
+    {
+        countries {
+            languages {
+                name
+            }
+            name
+            currency
+            code
+            phone
+            capital
+            states {
+                name
+            }
+        }
+    }
+`;
+
 export default function CountryApi() {
-  const { loading, error, data } = useQuery(GET_COUNTRIES);
-  const [selectedCountry, setSelectedCountry] = useState(null);
+    const { loading, error, data } = useQuery(GET_COUNTRIES);
+    const [selectedCountry, setSelectedCountry] = useState(null);
 
-  if (loading)
+    const handleCountryClick = (country) => {
+        setSelectedCountry(country);
+    };
+
     return (
-      <Layout>
-        <p>Loading...</p>
-      </Layout>
+        <Layout>
+            <ApolloProvider client={client}>
+                <h2>Click on a country for more details:</h2>
+                <div className={countryStyles}>
+                    <div className={listStyles}>
+                        <ul css={{ background: 'white', fontFamily: 'Inter' }}>
+                            {loading && <p>Loading...</p>}
+                            {error && <p>Error :(</p>}
+                            {data && data.countries.map((country) => (
+                                <li
+                                    key={country.name}
+                                    onClick={() => handleCountryClick(country)}
+                                    className={listItemStyles(selectedCountry && selectedCountry.name === country.name)}
+                                >
+                                    {country.name}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    {selectedCountry && (
+                        <div className={selectedCountryStyles}>
+                            <h2>{selectedCountry?.name}</h2>
+                            <p>Capital: {selectedCountry?.capital}</p>
+                            <p>Currency: {selectedCountry?.currency}</p>
+                            <p>Language: {selectedCountry?.languages[0]?.name}</p>
+                        </div>
+                    )}
+                </div>
+            </ApolloProvider>
+        </Layout>
     );
-  if (error)
-    return (
-      <Layout>
-        <p>Error :(</p>
-      </Layout>
-    );
-
-  const handleCountryClick = (country) => {
-    setSelectedCountry(country);
-    console.log(selectedCountry);
-  };
-
-  return (
-    <Layout>
-      <ApolloProvider client={client}>
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'center',
-          }}
-        >
-          <div style={{ width: '50%' }}>
-            <h2>Click on a country for morea details:</h2>
-            <ul style={{ background: 'white', fontFamily: 'Inter' }}>
-              {data.countries.map((country) => (
-                <li
-                  key={country.name}
-                  onClick={() => handleCountryClick(country)}
-                  style={{
-                    color: selectedCountry && selectedCountry.name === country.name ? 'blue' : 'none',
-                    cursor: 'pointer',
-                  }}
-                >
-                  {country.name}
-                </li>
-              ))}
-            </ul>
-          </div>
-          {selectedCountry && (
-            <div style={{ background: 'white', fontFamily: 'Inter' }}>
-              <h2>{selectedCountry.name}</h2>
-              <p>Capital: {selectedCountry.capital}</p>
-              <p>Currency: {selectedCountry.currency}</p>
-              <p>Language: {selectedCountry.languages[0].name}</p>
-            </div>
-          )}
-        </div>
-      </ApolloProvider>
-    </Layout>
-  );
 }
